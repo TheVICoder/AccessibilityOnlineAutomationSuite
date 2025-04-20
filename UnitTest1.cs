79136d4c2c25743fe5a132d0c7e58695b0eccbeb
TheVICoder <josh.murphy1999@icloud.com>
2025-04-20 18:18:57 +0100

    Convert to Xunit from Nunit

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace AOATs;
+
+
+public class HomePageTests : PageTest
+{
+    [Fact]
+public async Task CheckWelcomeTitleIsVisible()
+    {
+        await Page.GotoAsync("https://accessibility.online/");
+
+        var TitleIsVisible = await Page.Locator("//h1[text()='Wecome to the technology demo of accessibility online']").IsVisibleAsync();
+         Assert.True(TitleIsVisible);
+    }
+}
