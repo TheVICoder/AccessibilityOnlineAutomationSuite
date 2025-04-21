a34d677987ecd092f7843a23d477458fde3f98b1
TheVICoder <josh.murphy1999@icloud.com>
2025-04-21 10:25:42 +0100

    update to pom structure

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace AOATs;
+
+
+public class HomePageTests : PageTest
+{
+    [Fact]
+public async Task CheckWelcomeTitleIsVisible()
+    {
+var page = new HomePage(Page);
+await page.GotoAsync();
+
+
+        Assert.True(await page.CheckWelcomeHeadingIsVisible());
+    }
+}
